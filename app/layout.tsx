ee40711c19c48d2599e22e2c237c5f6a9a22bcaa
varmiguemunoz <varmiguemunoz@gmail.com>
2025-11-14 14:56:22 -0500

    Feature new vercel metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
@@ -9,7 +11,8 @@ import { SessionProvider } from "next-auth/react";
 export const metadata: Metadata = {
   metadataBase: new URL("https://chat.vercel.ai"),
   title: "Welthra - Your Personal AI Financial Assistant",
-  description: "Welthra is your personal AI financial assistant. It helps you manage your finances, track your spending, and make informed decisions about your money.",
+  description:
+    "Welthra is your personal AI financial assistant. It helps you manage your finances, track your spending, and make informed decisions about your money.",
 };
 
 export const viewport = {
@@ -56,10 +59,6 @@ export default function RootLayout({
   return (
     <html
       className={`${geist.variable} ${geistMono.variable}`}
-      // `next-themes` injects an extra classname to the body element to avoid
-      // visual flicker before hydration. Hence the `suppressHydrationWarning`
-      // prop is necessary to avoid the React hydration mismatch warning.
-      // https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
       lang="en"
       suppressHydrationWarning
     >
@@ -81,6 +80,8 @@ export default function RootLayout({
           <Toaster position="top-center" />
           <SessionProvider>{children}</SessionProvider>
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
